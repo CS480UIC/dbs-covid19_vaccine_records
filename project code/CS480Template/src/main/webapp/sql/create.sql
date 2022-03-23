a4f7c4969018b83c15485d749f40c61919c75660
wkopec5 <77519227+wkopec5@users.noreply.github.com>
2022-03-23 16:00:39 -0500

    Updated Dose 1 and Address
    Updated Dose 1 and Address

--- project code/CS480Template/src/main/webapp/sql/create.sql
+++ project code/CS480Template/src/main/webapp/sql/create.sql
@@ -12,6 +12,19 @@ CREATE TABLE patient(
 	PRIMARY KEY (patient_id)
 );
 
+CREATE TABLE address(
+    patient_id INT UNIQUE NOT NULL,
+    street_address VARCHAR(50) NOT NULL,
+    zip_code INT NOT NULL,
+    city VARCHAR(50) NOT NULL,
+    state CHAR(2) NOT NULL,
+    
+    PRIMARY KEY(patient_id, street_address),
+    FOREIGN KEY(patient_id)
+		REFERENCES patient(patient_id)
+		ON DELETE CASCADE
+);
+
 CREATE TABLE contact(
 	email_address VARCHAR(50) NOT NULL,
 	phone_num INT NOT NULL,
@@ -42,9 +55,29 @@ CREATE TABLE primary_physician(
 
 
 CREATE TABLE medical_history(
+    patient_id INT NOT NULL UNIQUE,
 	notes VARCHAR(200),
 	pre_existing_conditions VARCHAR(200),
 	contracted_covid DATE,
+    
+    PRIMARY KEY(patient_id),
+	FOREIGN KEY(patient_id)
+		REFERENCES patient(patient_id)
+		ON DELETE CASCADE
+);
+
+CREATE TABLE dose_1(
+    patient_id INT NOT NULL UNIQUE,
+    type_1 VARCHAR(10) NOT NULL CHECK(type_1 IN ('Moderna', 'Pfizer', 'J&J')),
+	lot_num_1 VARCHAR(10) NOT NULL,
+	dose_1_date DATE NOT NULL,
+	location_1 VARCHAR(50) NOT NULL,
+	
+	PRIMARY KEY (patient_id, lot_num_1, dose_1_date),
+	FOREIGN KEY(patient_id)
+		REFERENCES patient(patient_id)
+		ON DELETE CASCADE
+
 );
 
 CREATE TABLE dose_2(
@@ -52,8 +85,14 @@ CREATE TABLE dose_2(
 	lot_num_2 VARCHAR(10) NOT NULL,
 	dose_2_date DATE NOT NULL,
 	location_2 VARCHAR(50) NOT NULL,
-	
-	PRIMARY KEY (lot_num_2, dose_2_date)
+	lot_num_1 VARCHAR(10) NOT NULL,
+	dose_1_date DATE NOT NULL,
+    
+	PRIMARY KEY (lot_num_2, dose_2_date, lot_num_1, dose_1_date),
+    FOREIGN KEY (lot_num_1, dose_1_date)
+		REFERENCES dose_1(lot_num_1, dose_1_date)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE
 )
 
 CREATE TABLE booster(
@@ -64,7 +103,7 @@ CREATE TABLE booster(
 	dose_b_date DATE NOT NULL,
 	location_b VARCHAR(50) NOT NULL,
 	
-	PRIMARY KEY (lot_num_b, dose_b_date),
+	PRIMARY KEY (lot_num_b, dose_b_date, lot_num_2, dose_2_date),
 	FOREIGN KEY (lot_num_2, dose_2_date)
 		REFERENCES dose_2(lot_num_2, dose_2_date)
 		ON UPDATE CASCADE
