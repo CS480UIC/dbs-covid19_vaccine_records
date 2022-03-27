1722cd3ddf2df189640f8a2b357280164867f536
Manasvi Narayanan <mnaray5@uic.edu>
2022-03-26 22:59:06 -0500

    Update create.sql

--- project code/CS480Template/src/main/webapp/sql/create.sql
+++ project code/CS480Template/src/main/webapp/sql/create.sql
@@ -3,32 +3,32 @@ CREATE database covid19_vaccine_records;
 use covid19_vaccine_records;
 
 CREATE TABLE patient(
+	patient_id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	first_name VARCHAR(50) NOT NULL,
 	last_name VARCHAR(50) NOT NULL,
 	dob DATE NOT NULL,
 	gender CHAR(1) NOT NULL,
-	patient_id INT UNIQUE NOT NULL AUTO_INCREMENT,
 	
 	PRIMARY KEY (patient_id)
 );
 
 CREATE TABLE address(
-    patient_id INT UNIQUE NOT NULL,
-    street_address VARCHAR(50) NOT NULL,
-    zip_code INT NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    state CHAR(2) NOT NULL,
+    	patient_id INT UNIQUE NOT NULL,
+    	street_address VARCHAR(50) NOT NULL,
+    	zip_code INT NOT NULL,
+    	city VARCHAR(50) NOT NULL,
+   	state CHAR(2) NOT NULL,
     
-    PRIMARY KEY(patient_id, street_address),
-    FOREIGN KEY(patient_id)
+    	PRIMARY KEY(patient_id, street_address),
+    	FOREIGN KEY(patient_id)
 		REFERENCES patient(patient_id)
 		ON DELETE CASCADE
 );
 
 CREATE TABLE contact(
+	patient_id INT UNIQUE NOT NULL,
 	email_address VARCHAR(50) NOT NULL,
 	phone_num INT NOT NULL,
-	patient_id INT UNIQUE NOT NULL,
 	
 	PRIMARY KEY(patient_id),
 	FOREIGN KEY(patient_id)
@@ -55,20 +55,20 @@ CREATE TABLE primary_physician(
 
 
 CREATE TABLE medical_history(
-    patient_id INT NOT NULL UNIQUE,
+    	patient_id INT NOT NULL UNIQUE,
 	notes VARCHAR(200),
 	pre_existing_conditions VARCHAR(200),
 	contracted_covid DATE,
     
-    PRIMARY KEY(patient_id),
+    	PRIMARY KEY(patient_id),
 	FOREIGN KEY(patient_id)
 		REFERENCES patient(patient_id)
 		ON DELETE CASCADE
 );
 
 CREATE TABLE dose_1(
-    patient_id INT NOT NULL UNIQUE,
-    type_1 VARCHAR(10) NOT NULL CHECK(type_1 IN ('Moderna', 'Pfizer', 'J&J')),
+    	patient_id INT NOT NULL UNIQUE,
+	type_1 VARCHAR(10) NOT NULL CHECK(type_1 IN ('Moderna', 'Pfizer', 'J&J')),
 	lot_num_1 VARCHAR(10) NOT NULL,
 	dose_1_date DATE NOT NULL,
 	location_1 VARCHAR(50) NOT NULL,
@@ -81,31 +81,29 @@ CREATE TABLE dose_1(
 );
 
 CREATE TABLE dose_2(
+	patient_id INT NOT NULL UNIQUE,
 	type_2 VARCHAR(10) NOT NULL CHECK(type_2 IN ('Moderna', 'Pfizer', 'J&J')),
 	lot_num_2 VARCHAR(10) NOT NULL,
 	dose_2_date DATE NOT NULL,
 	location_2 VARCHAR(50) NOT NULL,
-	lot_num_1 VARCHAR(10) NOT NULL,
-	dose_1_date DATE NOT NULL,
     
-	PRIMARY KEY (lot_num_2, dose_2_date, lot_num_1, dose_1_date),
-    FOREIGN KEY (lot_num_1, dose_1_date)
-		REFERENCES dose_1(lot_num_1, dose_1_date)
+	PRIMARY KEY (patient_id),
+   	FOREIGN KEY (patient_id)
+		REFERENCES dose_1(patient_id)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
 )
 
 CREATE TABLE booster(
-	lot_num_2 VARCHAR(10) NOT NULL,
-	dose_2_date DATE NOT NULL,
+	patient_id INT NOT NULL UNIQUE,
 	type_b VARCHAR(10) NOT NULL CHECK(type_2 IN ('Moderna', 'Pfizer', 'J&J')),
 	lot_num_b VARCHAR(10) NOT NULL,
 	dose_b_date DATE NOT NULL,
 	location_b VARCHAR(50) NOT NULL,
 	
-	PRIMARY KEY (lot_num_b, dose_b_date, lot_num_2, dose_2_date),
-	FOREIGN KEY (lot_num_2, dose_2_date)
-		REFERENCES dose_2(lot_num_2, dose_2_date)
+	PRIMARY KEY (patient_id),
+	FOREIGN KEY (patient_id)
+		REFERENCES dose_2(patient_id)
 		ON UPDATE CASCADE
 		ON DELETE CASCADE
 )
