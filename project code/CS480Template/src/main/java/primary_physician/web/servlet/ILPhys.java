7639d82576d127842512433c3c53d9bba3e1925e
patel601 <55375652+patel601@users.noreply.github.com>
2022-04-22 14:17:11 -0500

    Create ILPhys.java

---
+++ project code/CS480Template/src/main/java/primary_physician/web/servlet/ILPhys.java
@@ -0,0 +1,36 @@
+package primary_physician.service;
+
+
+import java.util.List;
+
+import primary_physician.dao.Primary_physicianDao;
+import primary_physician.domain.Primary_physician;
+
+/**
+ * logic functions such as register, login
+ * @author Priyanka Patel
+ *
+ */
+public class Primary_physicianService {
+	private Primary_physicianDao primary_physicianDao = new Primary_physicianDao();
+	
+	/**
+	 * register a Entity1
+	 * @param form
+	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 */
+	public void create(Primary_physician form) throws Primary_physicianException, ClassNotFoundException, InstantiationException, IllegalAccessException{
+		// check the primary key of Entity1
+		Primary_physician entity1 = primary_physicianDao.findByID(form.getPatient_id());
+		if(entity1.getPatient_id()!=null && entity1.getPatient_id() == form.getPatient_id()) throw new Primary_physicianException("This primary physician has been registered!");
+		primary_physicianDao.add(form);
+	}
+	
+	public List<Object> findILPhys() throws InstantiationException, IllegalAccessException, ClassNotFoundException{
+		return primary_physicianDao.findILPhys();
+		
+	}
+
+}
